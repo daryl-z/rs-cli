b47a3c32243eda18b4da99d604ec312de619fa1b
Edward <3zangelks@gmail.com>
2025-08-09 00:24:53 +0800

    feat: rm player

--- src/process.rs
+++ src/process.rs
@@ -1,28 +1,51 @@
 use anyhow::Result;
-use csv::Reader;
-use serde::{Deserialize, Serialize};
+use csv::{Reader, StringRecord};
+// use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs::write;
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    #[serde(rename = "Name")]
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit_number: u8,
-}
+// #[derive(Debug, Serialize, Deserialize)]
+// #[serde(rename_all = "PascalCase")]
+// struct Player {
+//     #[serde(rename = "Name")]
+//     name: String,
+//     position: String,
+//     #[serde(rename = "DOB")]
+//     dob: String,
+//     nationality: String,
+//     #[serde(rename = "Kit Number")]
+//     kit_number: u8,
+// }
 
 pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let records = reader
-        .deserialize::<Player>()
-        .collect::<Result<Vec<Player>, _>>()?;
-    println!("CSV Options: {:?}", records);
-    let json = serde_json::to_string(&records)?;
-    write(output, json)?;
+    let headers: StringRecord = reader.headers()?.clone();
+    // let mut json_rows: Vec<Value> = Vec::new();
+    // for record_result in reader.records() {
+    //     let record = record_result?;
+    //     let mut obj = serde_json::Map::with_capacity(headers.len());
+    //     for (key, value) in headers.iter().zip(record.iter()) {
+    //         obj.insert(key.to_string(), Value::String(value.to_string()));
+    //     }
+    //     json_rows.push(Value::Object(obj));
+    // }
+
+    let rows: Vec<Value> = reader
+        .records()
+        .map(|record_res| {
+            record_res.map(|record| {
+                Value::Object(
+                    headers
+                        .iter()
+                        .zip(record.iter())
+                        .map(|(k, v)| (k.to_string(), Value::String(v.to_string())))
+                        .collect(),
+                )
+            })
+        })
+        .collect::<Result<Vec<_>, csv::Error>>()?;
+
+    let json_text = serde_json::to_string_pretty(&rows)?;
+    write(output, json_text)?;
     Ok(())
 }
