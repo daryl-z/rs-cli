808e85701ec111120c7acf1b09a9532b7c0e5f0a
Edward <3zangelks@gmail.com>
2025-08-08 21:58:45 +0800

    feat: read file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 use clap::Parser;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use std::path::Path;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Player {
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit_number: u8,
+}
 
 #[derive(Debug, Parser)]
 #[clap(
@@ -21,7 +38,7 @@ enum SubCommand {
 
 #[derive(Debug, Parser)]
 struct CsvOpts {
-    #[arg(short, long)]
+    #[arg(short, long, value_parser = verify_input_file)]
     input: String,
     #[arg(short, long, default_value = "output.json")]
     output: String,
@@ -31,27 +48,51 @@ struct CsvOpts {
     header: bool,
 }
 
-fn main() {
+fn verify_input_file(file: &str) -> Result<String, String> {
+    if Path::new(file).exists() {
+        Ok(file.to_string())
+    } else {
+        Err(format!("文件 '{}' 不存在", file))
+    }
+}
+
+fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let mut reader = Reader::from_path(opts.input)?;
+
+            // let records = reader.deserialize().map(|record| record.unwrap());
+            // let records = reader.deserialize::<Player>().collect()?;
+            let records = reader
+                .deserialize::<Player>()
+                .collect::<Result<Vec<Player>, _>>()?;
+            println!("CSV Options: {:?}", records);
+        }
+    }
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempfile::NamedTempFile;
     #[test]
     fn test_csv_opts() {
+        let tmp = NamedTempFile::new().expect("failed to create temp file");
+        let tmp_path = tmp.path().to_str().expect("temp path utf8").to_string();
+
         let opts = CsvOpts::parse_from([
             "csv",
             "--input",
-            "input.csv",
+            tmp_path.as_str(),
             "--output",
             "output.json",
             "--delimiter",
             ";",
             "--header",
         ]);
-        assert_eq!(opts.input, "input.csv");
+        assert_eq!(opts.input, tmp.path().to_str().unwrap());
         assert_eq!(opts.output, "output.json");
         assert_eq!(opts.delimiter, ';');
         assert!(opts.header);
