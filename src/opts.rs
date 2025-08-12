c4080a287fa6575e6b3a04f7261d41c588d9c6d5
Edward <3zangelks@gmail.com>
2025-08-12 19:31:40 +0800

    feat: add action & value_parser

--- src/opts.rs
+++ src/opts.rs
@@ -49,13 +49,13 @@ pub struct CsvOpts {
 pub struct GenPassOpts {
     #[arg(short, long, default_value_t = 12)]
     pub length: usize,
-    #[arg(long, default_value_t = true)]
+    #[arg(long, default_value_t = true, action = clap::ArgAction::Set, value_parser = clap::value_parser!(bool))]
     pub uppercase: bool,
-    #[arg(long, default_value_t = true)]
+    #[arg(long, default_value_t = true, action = clap::ArgAction::Set, value_parser = clap::value_parser!(bool))]
     pub lowercase: bool,
-    #[arg(long, default_value_t = true)]
+    #[arg(long, default_value_t = true, action = clap::ArgAction::Set, value_parser = clap::value_parser!(bool))]
     pub numbers: bool,
-    #[arg(long, default_value_t = true)]
+    #[arg(long, default_value_t = true, action = clap::ArgAction::Set, value_parser = clap::value_parser!(bool))]
     pub symbols: bool,
 }
 
